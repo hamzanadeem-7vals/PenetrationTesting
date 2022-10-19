7fcd6ac817e542cde7d124d59c88f876b30c27ff
hamza nadeem <hamza.nadeem@7vals.com>
2022-10-19 15:46:38 +0500

    fixed syntax

--- URL-BruteForce/ffufer.sh
+++ URL-BruteForce/ffufer.sh
@@ -4,5 +4,5 @@ file="$1";
 
 for line in $(cat "$file");
 do 
-    ffuf -r -ic -w /usr/share/wordlists/dirb/big.txt -o $line.txt -e .rb,.sql -u https://$line/FUZZ 
+    ffuf -r -ic -recursion -recursion-depth 5 -w /usr/share/wordlists/dirb/big.txt -o $line.txt -e .rb,.sql -u https://$line/FUZZ 
 done 
